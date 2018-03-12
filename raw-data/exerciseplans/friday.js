1498a277016bb9826faa8b20510430356d39504d
Swolebrain <thecodingteacher@gmail.com>
2018-03-12 18:59:44 -0400

    feat: improved data model for exercise plans

--- raw-data/exerciseplans/friday.js
+++ raw-data/exerciseplans/friday.js
@@ -1,51 +1,16 @@
 module.exports =  {
-  "caloriedeficit": {
-    "food": {
-      "goal": 400,
-      "current": 212,
-      "status": 1,
-      "color": "red",
-      "unit": "cal"
-    },
-    "exercise": {
-      "goal": 300,
-      "current": 212,
-      "status": 2,
-      "color": "amber",
-      "unit": "cal"
-    },
-    "activity": {
-      "goal": 100,
-      "current": 100,
-      "status": 3,
-      "color": "green",
-      "unit": "cal"
-    }
-  },
   "exerciselist": [
     {
-      "itemname": "Push-ups",
-      "foodsection": "bf",
+      "itemname": "Walking",
       "quantity": 1,
       "duration": 3600,
-      "calories": 150,
-      "macronutrients": [
-        {
-          "type": "carbs",
-          "percentage": 40,
-          "color": "green"
-        },
-        {
-          "type": "proteins",
-          "percentage": 50,
-          "color": "green"
-        },
-        {
-          "type": "fatgrams",
-          "percentage": 10,
-          "color": "red"
-        }
-      ]
+      "calories": 150 + Math.floor(Math.random() * 100),
+    },
+    {
+      "itemname": "running",
+      "quantity": 1,
+      "duration": 1800,
+      "calories": 350 + Math.floor(Math.random()*100),
     }
   ]
 };
