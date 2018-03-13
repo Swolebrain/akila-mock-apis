16d0b97d439a6fc330da30ac70a891d21dddbbe1
swolebrain <thecodingteacher@gmail.com>
2018-03-13 15:31:28 -0400

    feat: added icon and color information to mock API

--- raw-data/insights/summary.js
+++ raw-data/insights/summary.js
@@ -1,32 +1,80 @@
 module.exports =  [
   {
     "type": "Weight Loss",
-    "patterns": "Probability of you succeeding in meeting your goal and losing 12 pounds in 16 weeks is 70%.",
-    "focus": "To increase the chances please focus on more exercise and fewer calories on Friday and Saturday."
+    "patterns": {
+      "text": "Probability of you succeeding in meeting your goal and losing 12 pounds in 16 weeks is 70%.",
+      "insighticon": "md-thumbs-up", //fa-exclamation or md-thumbs-up
+      "iconcolor": "#96DC65", //green 96dc65, yellow F4CF5E, red E87357
+    },
+    "focus": {
+      "text": "To increase the chances please focus on more exercise and fewer calories on Friday and Saturday.",
+      "insighticon": "fa-exclamation", //fa-exclamation or md-thumbs-up
+      "iconcolor": "#E87357", //green 96dc65, yellow F4CF5E, red E87357
+    }
   },
   {
     "type": "Health Index",
-    "patterns": "You are doing great with the health index component of weight loss. However you have been consuming high GI foods and not exercising regularly",
-    "focus": "Increase your health index by using meal plan and shopping ahead of the week. Also try to meet your exercise goals"
+    "patterns": {
+      "text": "You are doing great with the health index component of weight loss. However you have been consuming high GI foods and not exercising regularly",
+      "insighticon": "fa-exclamation", //fa-exclamation or fa-exclamation
+      "iconcolor": "#F4CF5E", //green 96dc65, yellow F4CF5E, red E87357
+    },
+    "focus": {
+      "text": "Increase your health index by using meal plan and shopping ahead of the week. Also try to meet your exercise goals",
+      "insighticon": "fa-exclamation", //fa-exclamation or fa-exclamation
+      "iconcolor": "#E87357", //green 96dc65, yellow F4CF5E, red E87357
+    }
   },
   {
     "type": "Activity",
-    "patterns": "Coaching is helping you stay active throughout the week days. High intense activity is contributing to weight loss",
-    "focus": "Focus on weekends and get in more activity. Focus on intensity of activity and get it up"
+    "patterns": {
+      "text": "Coaching is helping you stay active throughout the week days. High intense activity is contributing to weight loss",
+      "insighticon": "md-thumbs-up", //fa-exclamation or 
+      "iconcolor": "#96DC65", //green 96dc65, yellow F4CF5E, red E87357
+    },
+    "focus": {
+      "text": "Focus on weekends and get in more activity. Focus on intensity of activity and get it up",
+      "insighticon": "fa-exclamation", //fa-exclamation or md-thumbs-up
+      "iconcolor": "#E87357", //green 96dc65, yellow F4CF5E, red E87357
+    }
   },
   {
     "type": "Exercise",
-    "patterns": "Coaching is helping you exercise throughout the week. High intense exercise is contributing to weight loss.",
-    "focus": "Focus on increasing the intensity and heart rate."
+    "patterns": {
+      "text": "Coaching is helping you exercise throughout the week. High intense exercise is contributing to weight loss.",
+      "insighticon": "md-thumbs-up", //fa-exclamation or md-thumbs-up
+      "iconcolor": "#96DC65", //green 96dc65, yellow F4CF5E, red E87357
+    },
+    "focus": {
+      "text": "Focus on increasing the intensity and heart rate.",
+      "insighticon": "fa-exclamation", //fa-exclamation or fa-exclamation
+      "iconcolor": "#E87357", //green 96dc65, yellow F4CF5E, red E87357
+    }
   },
   {
     "type": "Sleep",
-    "patterns": "You sleapt well the days you exercised and your health index was high with good nutritious food.",
-    "focus": "Keep the focus on exercise and nutrition."
+    "patterns": {
+      "text": "You sleapt well the days you exercised and your health index was high with good nutritious food.",
+      "insighticon": "fa-exclamation", //fa-exclamation or fa-exclamation
+      "iconcolor": "#F4CF5E", //green 96dc65, yellow F4CF5E, red E87357
+    },
+    "focus": {
+      "text": "Keep the focus on exercise and nutrition.",
+      "insighticon": "fa-exclamation", //fa-exclamation or fa-exclamation
+      "iconcolor": "#E87357", //green 96dc65, yellow F4CF5E, red E87357
+    }
   },
   {
     "type": "Stress",
-    "patterns": "Your stress levels are higher in the morning.",
-    "focus": "Starting the day with exercise and rotines can help."
+    "patterns": {
+      "text": "Your stress levels are higher in the morning.",
+      "insighticon": "fa-exclamation", //fa-exclamation or fa-exclamation
+      "iconcolor": "#F4CF5E", //green 96dc65, yellow F4CF5E, red E87357
+    },
+    "focus": {
+      "text": "Starting the day with exercise and rotines can help.",
+      "insighticon": "fa-exclamation", //fa-exclamation or fa-exclamation
+      "iconcolor": "#F4CF5E", //green 96dc65, yellow F4CF5E, red E87357
+    }
   }
 ];
